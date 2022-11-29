57a9ecac53f127ec5dd89a06cb91ba3ded9b82a5
mauricio pantoja <mauriciop173@hotmail.com>
2022-11-28 20:32:10 -0500

    Se agrega el proyecto

---
+++ src/main/java/com/plazit/market/PlazitMarketApplication.java
@@ -0,0 +1,13 @@
+package com.plazit.market;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlazitMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlazitMarketApplication.class, args);
+	}
+
+}
